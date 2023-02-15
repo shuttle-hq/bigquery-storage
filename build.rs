e540b4abd816e2314f4dbe70beb0430ab79cc030
unvalley <kirohi.code@gmail.com>
2023-02-15 12:08:49 +0900

    chore: try bump some crates

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().format(false).compile(
+    tonic_build::configure().compile(
         &[
             "googleapis/google/cloud/bigquery/storage/v1/arrow.proto",
             "googleapis/google/cloud/bigquery/storage/v1/avro.proto",
